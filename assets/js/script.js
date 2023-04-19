d6624c46e085d5996970a4b6ec76192c5a73f585
Anthony <denayeranthony1782@gmail.com>
2023-04-19 11:18:52 +0200

    Wip Thomas Delphine Anthony

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
-//link svg
-//https://www.svgrepo.com/show/309493/dark-theme.svg
-//https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png
+//Insert Svg Header
 
+let burgerMenu = document.querySelector(".menuBtn");
+let imgBurger = document.createElement("img");
+imgBurger.src =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png";
+burgerMenu.appendChild(imgBurger);
 
+let darkMode = document.querySelector(".darkmodeBtn");
+let imgDarkMode = document.createElement("img");
+imgDarkMode.src = "https://www.svgrepo.com/show/309493/dark-theme.svg";
+darkMode.appendChild(imgDarkMode);
 
 // create dishCard
 let libraryCard = document.querySelector(".libraryCard");
@@ -20,4 +27,26 @@ dishCollection.forEach((element) => {
   <button class="btnAdd">Add</button>`;
   newCard.innerHTML += content;
   libraryCard.appendChild(newCard);
+});
+
+function createMenuDishStyle() {
+  let header = document.querySelector("header");
+  let menu = document.createElement("div").classList.add("menu");
+  let content = `
+    <ul class="menuItems">
+  <li class="menuItem">Vegan</li>
+  <li class="menuItem">Comfort Food</li>
+  <li class="menuItem">Halal</li>
+  <li class="menuItem">Healthy</li>
+  <li class="menuItem">Nicolas Cage</li>
+</ul>
+    `;
+  menu.innerHTML += content;
+  header.appendChild(menu);
+}
+
+burgerMenu.addEventListener("click", createMenuDishStyle => {
+
+    console.log("hello");
+
 });
