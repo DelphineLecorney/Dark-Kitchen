f29bbefdd523de08fb0115336349ce6a100a541a
ThomasMoerman <thomas.moerman.7@gmail.com>
2023-04-19 14:01:07 +0200

    Fonction de tri JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,77 @@
 //Insert Svg Header
 
-let burgerMenu = document.querySelector(".menuBtn");
-let imgBurger = document.createElement("img");
-imgBurger.src =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png";
-burgerMenu.appendChild(imgBurger);
+// let burgerMenu = document.querySelector(".menuBtn");
+// let imgBurger = document.createElement("img");
+// imgBurger.src =
+//   "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png";
+// burgerMenu.appendChild(imgBurger);
+// burgerMenu.classList.add("burgerImg")
 
-let darkMode = document.querySelector(".darkmodeBtn");
-let imgDarkMode = document.createElement("img");
-imgDarkMode.src = "https://www.svgrepo.com/show/309493/dark-theme.svg";
-darkMode.appendChild(imgDarkMode);
+// let darkMode = document.querySelector(".darkmodeBtn");
+// let imgDarkMode = document.createElement("img");
+// imgDarkMode.src = "https://www.svgrepo.com/show/309493/dark-theme.svg";
+// darkMode.appendChild(imgDarkMode);
+// darkMode.classList.add("btnDarkMode");
 
 // create dishCard
 const libraryCard = document.querySelector(".libraryCard");
 
 dishCollection.forEach((element) => {
-  const newCard = document.createElement("div");
-  newCard.classList = "dishCard";
+    const newCard = document.createElement("div");
+    newCard.classList = "dishCard";
 
-  const ingredientsList = element.dishIngredients
-    .map((ingredient) => `<li>${ingredient}</li>`)
-    .join("");
+    const ingredientsList = element.dishIngredients
+        .map((ingredient) => `<li>${ingredient}</li>`)
+        .join("");
 
-  const content = `
+    const content = `
 <h2 class="dishTitle">${element.dishTitle}</h2>
 <img src="${element.dishImg}" alt="${element.dishTitle}" class="dishImg" />
 <p class="dishStyle">${element.dishStyle}</p>
 <ul class="dishIngredients">${ingredientsList}</ul>
-<p class="dishPrice">${element.dishPrice}</p>
+<p class="dishPrice">${element.dishPrice} €</p>
 <button class="btnAdd">Add</button>
 `;
 
-  newCard.innerHTML += content;
-  libraryCard.appendChild(newCard);
+    newCard.innerHTML += content;
+    libraryCard.appendChild(newCard);
 });
 
-function createMenuDishStyle() {
-  let header = document.querySelector("header");
-  let menu = document.createElement("div").classList.add("menu");
-  let content = `
-  <label for="tri">Style :</label>
-  <select id="tri">
-  <option value="Vegan">Vegan</option>
-  <option value="ComfortFood">Comfort Food</option>
-  <option value="Halal">Halal</option>
-  <option value="Healthy">Healthy</option>
-  <option value="NicolasCage">Nicolas Cage</option>
-  </select>
-    `;
-  menu.innerHTML += content;
-  header.appendChild(menu);
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// Sélection du menu déroulant et du conteneur de résultats
+const menuDeroulant = document.getElementById("tri");
+const conteneurResultats = document.querySelector(".libraryCard");
+
+function mettreAJourResultats() {
+    // Suppression des résultats précédents
+    conteneurResultats.innerHTML = "";
+
+    // Filtrage des objets en fonction de la sélection du menu déroulant
+    const triSelectionne = menuDeroulant.value;
+    const objetsFiltres = dishCollection.filter(objet => objet.dishStyle === triSelectionne);
+
+    // Affichage des résultats filtrés
+    objetsFiltres.forEach(element => {
+        const newCard = document.createElement("div");
+        newCard.classList = "dishCard";
+
+        const ingredientsList = element.dishIngredients
+            .map((ingredient) => `<li>${ingredient}</li>`)
+            .join("");
+
+        const content = `
+  <h2 class="dishTitle">${element.dishTitle}</h2>
+  <img src="${element.dishImg}" alt="${element.dishTitle}" class="dishImg" />
+  <p class="dishStyle">${element.dishStyle}</p>
+  <ul class="dishIngredients">${ingredientsList}</ul>
+  <p class="dishPrice">${element.dishPrice} €</p>
+  <button class="btnAdd">Add</button>
+  `;
+
+        newCard.innerHTML += content;
+        libraryCard.appendChild(newCard)
+    });
 }
 
-burgerMenu.addEventListener("click", (createMenuDishStyle) => {
-  console.log("menu");
-});
+menuDeroulant.addEventListener("change", mettreAJourResultats);
